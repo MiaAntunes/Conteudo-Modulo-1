a47312bacce8d5e5ebaceff3d0b9abb4f84ff867
MiaAntunes <suportecamilaantunes2003@gmail.com>
2023-03-30 15:50:35 -0300

    Update

--- aula-11-Laços-II/lacos-II-exercicios-main/exercicio3/script.js
+++ aula-11-Laços-II/lacos-II-exercicios-main/exercicio3/script.js
@@ -1,36 +1,10 @@
 const rankingMaioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"];
+let indice = 0
 
-for(let indice of rankingMaioresPaises){
-    for(x=0; x<rankingMaioresPaises.length ; x++){
-        console.log(`${[x+1]}° - ${indice}`)
-    }
+for( let paises of rankingMaioresPaises){
+        // console.log(x)
+        // console.log(indice)
+        indice++
+        console.log(`${indice}° - ${paises}`)
 }
-
-/* Resposta:
-1° - Rússia
-2° - Rússia
-3° - Rússia
-4° - Rússia
-5° - Rússia
-1° - Canadá
-2° - Canadá
-3° - Canadá
-4° - Canadá
-5° - Canadá
-1° - China
-2° - China
-3° - China
-4° - China
-5° - China
-1° - EUA
-2° - EUA
-3° - EUA
-4° - EUA
-5° - EUA
-1° - Brasil
-2° - Brasil
-3° - Brasil
-4° - Brasil
-5° - Brasil
-
-*/
+    
